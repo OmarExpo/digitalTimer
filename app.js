2cb20a0de8c657df263d497eb4556ad9f08bce28
omar <omar1807@stud.kea.dk>
2022-02-14 22:42:35 +0100

    updates app

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get("/", (req,res) => {
 })
 
 
-const port = process.env.port || 3000;
-
+//const port = process.env.port || 3000;
+const port = 3000;
 
 app.listen(port, () => console.log("Server has started successfully!!"));
