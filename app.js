31f684bf4cda76e9ab909a22aa07624b69c36726
omar <omar1807@stud.kea.dk>
2022-02-14 23:18:13 +0100

    app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.static("./public"))
 
 app.get("/", function(req,res) 
 {
-  //res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
   //res.send("Welcome to RestAPI from nodeJS")
 })
 
